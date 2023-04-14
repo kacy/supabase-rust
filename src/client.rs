9e9da0463941a57ee33c769baff20d75db9cb7b4
Kacy Fortner <kacy@kacyfortner.com>
2023-04-14 12:03:46 -0400

    adding auth for jwt

--- src/client.rs
+++ src/client.rs
@@ -4,7 +4,9 @@ use reqwest::{Client};
 use crate::Supabase;
 
 impl Supabase {
-    pub fn new(url: Option<&str>, api_key: Option<&str>) -> Self {
+    // Creates a new Supabase client. If no parameters are provided, it will attempt to read the
+    // environment variables `SUPABASE_URL`, `SUPABASE_API_KEY`, and `SUPABASE_JWT_SECRET`.
+    pub fn new(url: Option<&str>, api_key: Option<&str>, jwt: Option<&str>) -> Self {
         let client: Client = Client::new();
         let url: String = url
             .map(String::from)
@@ -12,11 +14,15 @@ impl Supabase {
         let api_key: String = api_key
             .map(String::from)
             .unwrap_or_else(|| env::var("SUPABASE_API_KEY").unwrap_or_else(|_| String::new()));
+        let jwt: String = jwt
+            .map(String::from)
+            .unwrap_or_else(|| env::var("SUPABASE_JWT_SECRET").unwrap_or_else(|_| String::new()));
 
         Supabase {
             client,
             url: url.to_string(),
             api_key: api_key.to_string(),
+            jwt: jwt.to_string(),
             bearer_token: None,
         }
     }
@@ -28,8 +34,8 @@ mod tests {
 
     #[test]
     fn test_client() {
-        let client: Supabase = Supabase::new(None, None);
-        let url = std::env::var("SUPABASE_URL").unwrap_or_else(|_| String::new());
+        let client: Supabase = Supabase::new(None, None, None);
+        let url = client.url.clone();
         assert!(client.url == url);
     }
 }
